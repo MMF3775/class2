78db9a3dbc49ebc5563fd741630af51fdc2ab8e0
mmf3775 <mmf3775@gmail.com>
2024-08-05 19:13:16 +0330

    add province and city controller

---
+++ app/city/controller.py
@@ -0,0 +1,16 @@
+
+class CityController:
+    def __init__(self):
+        pass
+
+    # TODO T03 : gashtasbi
+    def create(self):
+        pass
+
+    # TODO T04 : shimohammadi
+    def update(self):
+        pass
+
+    # TODO T005 : zabihi
+    def delete(self):
+        pass
---
+++ app/province/controller.py
@@ -0,0 +1,16 @@
+
+class ProvinceController:
+    def __init__(self):
+        pass
+
+    # TODO T06 : Farahi
+    def create(self):
+        pass
+
+    # TODO T07 : forghani
+    def update(self):
+        pass
+
+    # TODO T08 : goli
+    def delete(self):
+        pass

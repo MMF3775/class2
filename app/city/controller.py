06d9fb80f4948f2ddfbc7a71caf614b916348673
yasamin-s96 <yasamins96@gmail.com>
2024-08-05 19:55:28 +0330

    #T04

--- app/city/controller.py
+++ app/city/controller.py
@@ -1,4 +1,4 @@
-
+from app.city.repository import CityRepository
 class CityController:
     def __init__(self):
         pass
@@ -8,8 +8,8 @@ class CityController:
         pass
 
     # TODO T04 : shimohammadi
-    def update(self):
-        pass
+    def update(self, city_id: str, name: str, province_id: str):
+        return CityRepository.update(name=name, city_id=city_id, province_id=province_id)
 
     # TODO T005 : zabihi
     def delete(self):
